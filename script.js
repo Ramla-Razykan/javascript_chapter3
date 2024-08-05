9963213761253c23492e6cfb2ce7ac8706eb7222
Ramla Eman <ramla.eman.ra@gmail.com>
2024-08-05 11:58:27 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Question 1: Declare a variable called age & assign to it your age. Show your age in an alert box. 
+// var age = 14;
+// alert("I am" + " " + age + " " + "Years old.");
+// console.log("I am" + " " + age + " " + "Years old.");
+
+// Question 2:Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+// var visit_Site;
+// visit_Site = 7;
+// alert("You have visited this site" + " " + visit_Site + " " + "times");
+// console.log("You have visited this site" + " " + visit_Site + " " + "times");
+
+// Question 3:Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser.
+// var birthYear = 2009;
+// document.write("My Birth year is" +" "+ birthYear + "<br/>");
+// console.log("My Birth year is" +" "+ birthYear + "<br/>");
+// document.write("Data type of my declared variable is number.");
+// console.log("Data type of my declared variable is number.");
+
+// Question 4: A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information: a. Visitor’s name b. Product    title c. Quantity i.e. how many products a visitor wants to order Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”. 
+// var visiterName = "John Doe";
+// var productTitle = "T-shirt"
+// var productQuntity = "5"
+// document.write(visiterName + " " + "ordered" + " " + productQuntity + " " + productTitle + "(s)" +  " " + "on XYZ Clothing store.");
+// console.log(visiterName + " " + "ordered" + " " + productQuntity + " " + productTitle + "(s)" +  " " + "on XYZ Clothing store.");
